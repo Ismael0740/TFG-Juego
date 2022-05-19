780f593f629a952b380ba247af565f1292a20b96
Ismael0740 <75542920+Ismael0740@users.noreply.github.com>
2022-05-19 10:55:59 +0200

    Subida trabajo existente
    Volcado de todo el proyecto hasta el dia de hoy (19/5/2022). La ultima modificación ha sido configurar las hitboxes de los mapas y npcs.
    El proyecto hasta aqui ya contiene las siguientes caracteristicas:
    
    - Creacion de mapas y colocacion y configuracion de los gameObjects en el mismo (aka los pjs)
    
    - Creacion del bucle de juego (gameLoop) que se ejecutara cada frame. CLASE PRINCIPAL (OVERWORLD)
    
    - Creacion de logica de movimiento de personaje
    
    - Creacion y configuracion de los sprites, otorgandole a los personajes animaciones en cada situacion
    
    - Creacion y configuracion de la camara, siempre mantiene al personaje principal en el centro del cavnas (podemos asociarle el centro del canvas a cualquier otra coordenada o personaje para simular que la camara sigue ese punto o esa entidad)
    
    - Creacion de logica de colisiones. Situados los limites de los 2 primeros mapas existentes hasta este punto. Tambien se ha creado la hitbox de los personajes, y se ha configurado dicha hitbox para desplazarse junto con el personaje en caso de que este se mueva, de esta manera se simula que la hitbox del personaje es constante independientemente de donde este y se vuelve mas organico el proceso.

---
+++ GameObject.js
@@ -0,0 +1,23 @@
+class GameObject {
+  constructor(config) {
+    this.isMounted = false;
+    this.x = config.x || 0;
+    this.y = config.y || 0;
+    this.direction = config.direction || "down";
+    this.sprite = new Sprite({
+      gameObject: this,
+      src: config.src || "/images/characters/people/hero.png",
+    });
+  }
+
+  mount(map){
+    console.log("monta")
+    this.isMounted = true;
+    map.addWall(this.x, this.y);
+  }
+
+
+  update() {
+
+  }
+}
---
+++ OverworldMap.js
@@ -0,0 +1,194 @@
+class OverworldMap {
+  constructor(config) {
+    this.gameObjects = config.gameObjects;
+    this.walls = config.walls || {};
+
+    this.lowerImage = new Image();
+    this.lowerImage.src = config.lowerSrc;
+
+    this.upperImage = new Image();
+    this.upperImage.src = config.upperSrc;
+  }
+
+  drawLowerImage(ctx, cameraPerson) {
+    ctx.drawImage(this.lowerImage,
+      utils.withGrid(10.5) - cameraPerson.x,
+      utils.withGrid(6) - cameraPerson.y
+      )
+  }
+
+  drawUpperImage(ctx, cameraPerson) {
+    ctx.drawImage(this.upperImage,
+      utils.withGrid(10.5) - cameraPerson.x,
+      utils.withGrid(6) - cameraPerson.y
+      )
+  } 
+
+  isSpaceTaken(currentX, currentY, direction){
+    const{x,y} = utils.nextPosition(currentX, currentY, direction);
+    return this.walls[`${x},${y}`] || false;
+  }
+
+  mountObjects(){
+    Object.values(this.gameObjects).forEach(o => {
+
+      //TODO: hacer que compruebe si el objeto deberia montarse o no
+
+      o.mount(this);
+    })
+  }
+
+  //añade un muro
+  addWall(x,y){
+    this.walls[`${x},${y}`] = true;
+  }
+
+  //borra un muro
+  removeWall(x, y){
+    delete this.walls[`${x},${y}`]
+  }
+
+  //mueve un muro
+  moveWall(wasX, wasY, direction){
+    this.removeWall(wasX, wasY); //borra el muro
+    const {x,y} = utils.nextPosition(wasX, wasY, direction); //crea uno nuevo en la siguiente posicion a donde lo habiamos borrado
+    this.addWall(x,y); //lo coloca
+  }
+
+}
+
+window.OverworldMaps = {
+  DemoRoom: {
+    lowerSrc: "/images/maps/DemoLower.png",
+    upperSrc: "/images/maps/DemoUpper.png",
+    gameObjects: {
+      hero: new Person({
+        isPlayerControlled: true,
+        x: utils.withGrid(5),
+        y: utils.withGrid(6),
+      }),
+      npc1: new Person({
+        x: utils.withGrid(7),
+        y: utils.withGrid(9),
+        src: "/images/characters/people/npc1.png"
+      })
+    },
+    walls: {
+      //mesa
+      [utils.asGridCoord(7,6)] : true,
+      [utils.asGridCoord(8,6)] : true,
+      [utils.asGridCoord(7,7)] : true,
+      [utils.asGridCoord(8,7)] : true,
+
+      //Paredes
+      [utils.asGridCoord(1,3)] : true,
+      [utils.asGridCoord(2,3)] : true,
+      [utils.asGridCoord(3,3)] : true,
+      [utils.asGridCoord(4,3)] : true,
+      [utils.asGridCoord(5,3)] : true,
+      [utils.asGridCoord(6,3)] : true,
+      [utils.asGridCoord(6,4)] : true,
+      [utils.asGridCoord(6,2)] : true,
+      [utils.asGridCoord(8,4)] : true,
+      [utils.asGridCoord(7,1)] : true,
+      [utils.asGridCoord(8,2)] : true,
+      [utils.asGridCoord(8,3)] : true,
+      [utils.asGridCoord(9,3)] : true,
+      [utils.asGridCoord(10,3)] : true,
+      [utils.asGridCoord(11,4)] : true,
+      [utils.asGridCoord(11,5)] : true,
+      [utils.asGridCoord(11,6)] : true,
+      [utils.asGridCoord(11,7)] : true,
+      [utils.asGridCoord(11,8)] : true,
+      [utils.asGridCoord(11,9)] : true,
+      [utils.asGridCoord(10,10)] : true,
+      [utils.asGridCoord(8,10)] : true,
+      [utils.asGridCoord(6,10)] : true,
+      [utils.asGridCoord(5,11)] : true,
+      [utils.asGridCoord(4,10)] : true,
+      [utils.asGridCoord(3,10)] : true,
+      [utils.asGridCoord(2,10)] : true,
+      [utils.asGridCoord(1,10)] : true,
+      [utils.asGridCoord(0,9)] : true,
+      [utils.asGridCoord(0,8)] : true,
+      [utils.asGridCoord(0,7)] : true,
+      [utils.asGridCoord(0,6)] : true,
+      [utils.asGridCoord(0,5)] : true,
+      [utils.asGridCoord(0,4)] : true,
+
+    }
+  },
+  Kitchen: {
+    lowerSrc: "/images/maps/KitchenLower.png",
+    upperSrc: "/images/maps/KitchenUpper.png",
+    gameObjects: {
+      hero: new Person({
+        isPlayerControlled: true,
+        x: utils.withGrid(5),
+        y: utils.withGrid(6)
+      }),
+      npcA: new Person({
+        x: utils.withGrid(9),
+        y: utils.withGrid(6),
+        src: "/images/characters/people/npc2.png"
+      }),
+      npcB: new Person({
+        x: utils.withGrid(10),
+        y: utils.withGrid(8),
+        src: "/images/characters/people/npc3.png"
+      })
+    },
+    walls: {
+      //mesas
+      [utils.asGridCoord(6,7)] : true,
+      [utils.asGridCoord(7,7)] : true,
+      [utils.asGridCoord(9,7)] : true,
+      [utils.asGridCoord(10,7)] : true,
+      [utils.asGridCoord(9,9)] : true,
+      [utils.asGridCoord(10,9)] : true,
+      [utils.asGridCoord(1,5)] : true,
+      [utils.asGridCoord(1,6)] : true,
+      [utils.asGridCoord(1,7)] : true,
+
+
+      //Paredes
+      [utils.asGridCoord(1,3)] : true,
+      [utils.asGridCoord(2,3)] : true,
+      [utils.asGridCoord(3,3)] : true,
+      [utils.asGridCoord(4,3)] : true,
+      [utils.asGridCoord(5,3)] : true,
+      [utils.asGridCoord(6,3)] : true,
+      [utils.asGridCoord(7,3)] : true,
+      [utils.asGridCoord(8,3)] : true,
+      [utils.asGridCoord(9,3)] : true,
+      [utils.asGridCoord(10,3)] : true,
+      [utils.asGridCoord(11,4)] : true,
+      [utils.asGridCoord(12,4)] : true,
+      [utils.asGridCoord(13,5)] : true,
+      [utils.asGridCoord(13,6)] : true,
+      [utils.asGridCoord(13,7)] : true,
+      [utils.asGridCoord(13,8)] : true,
+      [utils.asGridCoord(13,9)] : true,
+      [utils.asGridCoord(12,10)] : true,
+      [utils.asGridCoord(11,10)] : true,
+      [utils.asGridCoord(10,10)] : true,
+      [utils.asGridCoord(9,10)] : true,
+      [utils.asGridCoord(8,10)] : true,
+      [utils.asGridCoord(7,10)] : true,
+      [utils.asGridCoord(6,10)] : true,
+      [utils.asGridCoord(5,11)] : true,
+      [utils.asGridCoord(4,10)] : true,
+      [utils.asGridCoord(3,10)] : true,
+      [utils.asGridCoord(2,10)] : true,
+      [utils.asGridCoord(1,10)] : true,
+      [utils.asGridCoord(0,9)] : true,
+      [utils.asGridCoord(0,8)] : true,
+      [utils.asGridCoord(0,7)] : true,
+      [utils.asGridCoord(0,6)] : true,
+      [utils.asGridCoord(0,5)] : true,
+      [utils.asGridCoord(0,4)] : true,
+      
+    
+    },
+  }
+}
